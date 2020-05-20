48a2c8df8eb89825dcc7b16242d3ec8971acdb70
MohdShaqib97 <65538711+MohdShaqib97@users.noreply.github.com>
2020-05-20 18:53:32 +0530

    Update pythonDB.py

--- pythonDB.py
+++ pythonDB.py
@@ -3,15 +3,18 @@
 import mysql.connector
 import pandas as pd
 
+#Read csv file
 file = pd.read_csv("friends.csv")
 print(file)
 
-tb = [int(input(f"select table {i+1}:")) for i in range(4)]
+#Selection of Columns
+col = [int(input(f"select column {i+1}:")) for i in range(4)]
 head = [i for i in file]
 selected_columns = ['name']
-for i in tb:
+for i in col:
     selected_columns.append(head[i])
 
+#Selection of data from selected Columns
 l=[]
 t=[]
 for j in range(0,len(tb)):
@@ -20,16 +23,17 @@ for j in range(0,len(tb)):
     l.append(tuple(t))
     t.clear()
 
+#Connection to mysql
 mydb = mysql.connector.connect(host="localhost", user="root",passwd="mysql@123", database="tutorial1")
 mycursor = mydb.cursor()
 
-
+#Executing Query(inserting the value from csv to mysql)
 for i in l:
     mycursor.execute(f"insert into marks values{i}")     # marks is the table name
     mydb.commit()
-    
-mycursor.execute("select * from marks")
 
+#Showing the data in output console    
+mycursor.execute("select * from marks")
 data = mycursor.fetchall()
 for i in data:
     print(i)
