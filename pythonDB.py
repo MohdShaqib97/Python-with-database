89a0f5e1c40fa33c8a03358bb921c3b4152899ad
MohdShaqib97 <65538711+MohdShaqib97@users.noreply.github.com>
2020-05-20 16:43:38 +0530

    Reading specific column of database

--- pythonDB.py
+++ pythonDB.py
@@ -6,12 +6,16 @@ import pandas as pd
 file = pd.read_csv("friends.csv")
 print(file)
 
+tb = [int(input(f"select table {i+1}:")) for i in range(4)]
 head = [i for i in file]
-print(head)
+selected_columns = ['name']
+for i in tb:
+    selected_columns.append(head[i])
+
 l=[]
 t=[]
-for j in range(0,len(file)):
-    for i in head:
+for j in range(0,len(tb)):
+    for i in selected_columns:
         t.append(file[i][j])
     l.append(tuple(t))
     t.clear()
@@ -21,7 +25,7 @@ mycursor = mydb.cursor()
 
 
 for i in l:
-    mycursor.execute(f"insert into marks values{i}")
+    mycursor.execute(f"insert into marks values{i}")     # marks is the table name
     mydb.commit()
     
 mycursor.execute("select * from marks")
